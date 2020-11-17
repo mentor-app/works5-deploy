4b832ad48761056914219a7569ba382f503e1bf8
dev-heeham <dev.heeham@gmail.com>
2020-11-17 13:54:24 +0900

    Add Home Redirection

--- heestagram/urls.py
+++ heestagram/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from home import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     path('accounts/', include('accounts.urls')),
